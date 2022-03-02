dac4fec711db75fd67c1181630853f477e20b94f
Edster <74716069+EdaAkaltun@users.noreply.github.com>
2022-03-03 00:02:29 +0100

    hij werkt nog omg

--- language.js
+++ language.js
@@ -16,22 +16,30 @@ link.forEach(el => {
 });
 
 var data = {
-	  "english": 
+	  "English": 
 	  {
-		"title": "Hello World",
+		"title": "About Me",
 		"description": 
-			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non adipisci eligendi repellat ad dolor veritatis itaque sequi minus iste, doloremque. Officiis non eaque atque excepturi repudiandae nulla eos eligendi magni molestiae eius distinctio, voluptas pariatur incidunt et culpa inventore aspernatur recusandae nihil asperiores, vitae, maiores laborum quasi perspiciatis natus dignissimos! Accusantium aliquam nostrum impedit dignissimos iste, iure inventore! Sapiente, labore earum ut dicta ducimus asperiores laudantium natus officiis, quisquam placeat aspernatur voluptatum aut voluptates tenetur quos magni fugit quia. Fugiat."
+			" My name is Eda Akaltun. I’m currently a first year MSc Building Technology student at the Technical University of Delft and in the meantime active in the Solar Decathlon team of my university as a Climate Systems engineer & contest champion for ventilation."
+             + "\n" +
+            " I am very enthusiastic about the digitalization of the construction sector, and I also love challenging myself through tackling complex projects."
+            + "\n" + 
+            "The impact that I wish to make with my provision, is to benefit others positively. Therefore, I find sustainability, charity, and healing architecture very important."
+            + "\n" + 
+            "In my free time I love entertaining the procrastination monkey in my brain through spending time on coding, gaming and graphic design. Or I simply chase my curiosity and find myself doing all sorts of different things such as: woodworking, cultivating, learning about psychology, map design, digital art or even app building."
+        },
+	  "Nederlands": 
+	  {
+		"title": "Over Mij",
+		"description": 
+			"..."
 	  },
-	  "kazakh": 
+	  "Türkçe": 
 	  {
-		"title": "Сәлем Әлем",
+		"title": "Hakkımda",
 		"description": 
-			"Сәбіз Lorem ipsum, жеңілдік. Бұл ауырсыну үшін осы ұннан таңдамаңыз, сондықтан аз Осы ауырсынуды орындаңыз. Қызметтер жоқ және оның айырмашылығын ұлы ыңғайсыздық таңдау, тәжірибе ретінде қабылданған ештеңе өңдеу үшін нәтиже инцидент және қателіктерді зерттеуші ләззат, өмір, encounter born сияқты үлкен тағамдар-бәрі! Осы мәселе бойынша біздің сабоның кейбірін ашу, зерттеуші дұрыс! Дана, біз asperiores туған қызметтер тізімі деді олардың жұмыс, кез келген уақытта, содан бері үлкен жүгірістер, соның ішінде ләззат немесе рахат өңдеу сұраймыз. Қашуға."
-	  },
-	  "japanese": 
-	  {
-		"title": "ハロー・ワールド",
-		"description": 
-			"ﾂつｨﾂ知ﾂづｧﾂつｹﾂ-ﾂ新ﾂ陳ﾂ湘ｮﾂ陛ｱ これらの苦痛から真実の苦痛に選ぶために得てはいけない、従ってより少しにこの苦痛に従ってはいけない。 サービスではないと、彼の区別の大きな不快感から選択するそれらの否認を歓迎しない、結果として生じた事件の喜びと障害エクスプローラが受け入 このことについて私たちの下駄のいくつかを非難,右エクスプローラ! 的に仕事をしていまasperioresリストの生まれのサービスをつけてください編集喜びや楽しみの開催なども走りました。 走れ。."
+			".."
 	  }
 	}
+
+    htmlstring = stringContainingNewLines.replace(/(\r\n|\n|\r)/gm, "<br>");
